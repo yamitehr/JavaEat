e6cd0516710ee16a6338edfe3a57ea2b1b6de716
DESKTOP-O2L54PE\li493 <li49311@gmail.com>
2021-07-28 18:31:14 +0300

    added a manager dish screen and add dish screen and validation of register fields

--- SolEx2/src/Controllers/AddDishController.java
+++ SolEx2/src/Controllers/AddDishController.java
@@ -1,5 +1,99 @@
 package Controllers;
 
-public class AddDishController extends ControllerWrapper {
+import java.util.ArrayList;
 
-}
+import Exceptions.InvalidInputException;
+import Model.Component;
+import Model.Dish;
+import Model.Restaurant;
+import Utils.DishType;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class AddDishController extends ControllerWrapper {
+	@FXML
+	private TextField dish_Name;
+	@FXML
+	private ComboBox<DishType> typesBox;
+	@FXML
+	private TextField timeToMake;
+	@FXML
+	private ListView<Component> componentsList;
+	@FXML
+	private Text messageToUser;
+	
+	@FXML
+    public void initialize() {
+		init();
+    }
+	
+	private void init() {
+		ObservableList<DishType> types = FXCollections.observableArrayList(DishType.values());
+		typesBox.getItems().clear();				
+		typesBox.setItems(FXCollections.observableArrayList(types));
+		
+		ObservableList<Component> components = FXCollections.observableArrayList(Restaurant.getInstance().getComponenets().values());
+		componentsList.setItems(components);
+		componentsList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+	}
+	
+	public void moveToManagerDishScene(ActionEvent e) {
+		moveToScene("/View/Manager_Dish.fxml", (Stage)dish_Name.getScene().getWindow());
+	}
+	
+	public void addDish(ActionEvent e) {
+		try {
+			String dishName = dish_Name.getText();
+			if(dishName.isEmpty()) {
+				throw new InvalidInputException("Please fill dish name");
+			}
+			DishType dishType = typesBox.getValue();
+			if(dishType == null) {
+				throw new InvalidInputException("Please select Dish Type");
+			}
+			
+			int time;
+			if(timeToMake.getText().isEmpty()) {
+				throw new InvalidInputException("Please fill dish time to make");
+			}
+			else
+				time = Integer.parseInt(timeToMake.getText());
+			ArrayList<Component> dishComponents = new ArrayList<Component>();
+			dishComponents.addAll(componentsList.getSelectionModel().getSelectedItems());	
+			if(dishComponents.isEmpty())
+				throw new InvalidInputException("Please choose at least one component!");		   
+			Dish newDish = new Dish(dishName, dishType, dishComponents, time);
+			///		
+		
+			if(Restaurant.getInstance().addDish(newDish)) {
+				messageToUser.setFill(Color.BLUE);
+				messageToUser.setText("Dish added successfully");
+				dish_Name.clear();
+				timeToMake.clear();
+				typesBox.getSelectionModel().clearSelection();
+			}else {
+				messageToUser.setFill(Color.RED);
+				messageToUser.setText("an error has accured, please try again.");
+			}
+		}catch(InvalidInputException inputE) {
+			messageToUser.setFill(Color.RED);
+			messageToUser.setText(inputE.getMessage());
+		}catch(NumberFormatException ne) {
+			messageToUser.setFill(Color.RED);
+			messageToUser.setText("Wrong Input!");
+		}catch(Exception ex) {
+			messageToUser.setFill(Color.RED);
+			messageToUser.setText("an error has accured please try again");
+		}
+	}
+	}
+
--- SolEx2/src/Controllers/SignInController.java
+++ SolEx2/src/Controllers/SignInController.java
@@ -1,45 +1,36 @@
 package Controllers;
 
-import java.awt.ScrollPane;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.time.LocalDate;
 
+import Exceptions.InvalidPersonInputException;
 import Model.Customer;
 import Utils.Gender;
 import Utils.Neighberhood;
 import javafx.collections.FXCollections;
-
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class SignInController {
+public class SignInController extends ControllerWrapper {
 
 	private ObservableList<Neighberhood> neighberhood = FXCollections.observableArrayList(Neighberhood.values());
 	@FXML
-	private ChoiceBox<Neighberhood> NeighberhoodBox;
-	
+	private ChoiceBox<Neighberhood> NeighberhoodBox;	
 	@FXML
 	private void initialize() {
-		NeighberhoodBox.setValue(Neighberhood.DownTown);
 		NeighberhoodBox.setItems(neighberhood);
 	}
-	@FXML
-	private AnchorPane mainScreen;
 	@FXML
 	private TextField userName;
 	@FXML
@@ -61,43 +52,72 @@ public class SignInController {
 	@FXML
 	private PasswordField password;
 	@FXML
-	private Label check;
+	private Text messageToUser;
+	@FXML
+	private Button signInBtn;
 	
 	public void register(ActionEvent e) throws Exception {
-		Gender gender;
-		boolean sensitiveLactose = false;
-		boolean sensitiveGluten = false;
-		if(male.isSelected()) {
-			gender = Gender.Male;
+		try {
+			String user_Name = userName.getText();
+			if(user_Name.isEmpty()) {
+				throw new InvalidPersonInputException("Please fill User Name");
+			}
+			String first_Name = firstName.getText();
+			if(first_Name.isEmpty()) {
+				throw new InvalidPersonInputException("Please fill First Name");
+			}			
+			String last_Name = lastName.getText();
+			if(last_Name.isEmpty()) {
+				throw new InvalidPersonInputException("Please fill Last Name");
+			}
+			LocalDate date;
+			date = birthDate.getValue();
+			if(birthDate == null) {
+				throw new InvalidPersonInputException("Please select Date of Birth");
+			}
+			if(date.isAfter(LocalDate.now())) {
+				throw new InvalidPersonInputException("Date of birth can't be in the future");
+			}
+			Neighberhood neighberhood = NeighberhoodBox.getValue();
+			if(neighberhood == null) {
+				throw new InvalidPersonInputException("Please select Neighborhood");
+			}
+			Gender gender;
+			boolean sensitiveLactose = false;
+			boolean sensitiveGluten = false;
+			if(male.isSelected()) {
+				gender = Gender.Male;
+			}
+			else if(female.isSelected()) {
+				gender = Gender.Female;
+			}
+			else {
+				gender = Gender.Unknown;
+			}
+		
+			if(lactose.isSelected())
+				sensitiveLactose = true;
+			if(gluten.isSelected())
+				sensitiveGluten = true;
+		
+			Customer customer = new Customer(firstName.getText(), lastName.getText(), birthDate.getValue(), 
+					gender, NeighberhoodBox.getValue(), sensitiveLactose, sensitiveGluten, userName.getText(), password.getText());	
+			MainController.rest.addCustomer(customer);
+		
+			//new Alert(Alert.AlertType.INFORMATION, "Welcome " + firstName.getText() + ".\n Please log in.").showAndWait();
+			Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+			alert.setTitle("Welcome " + firstName.getText());
+			alert.setHeaderText("Welcome " + firstName.getText() + ". \n Please log in to continue.");
+			alert.showAndWait();
+		
+			moveToScene("/View/Login.fxml", (Stage)signInBtn.getScene().getWindow());
+		}catch(InvalidPersonInputException ipe) {
+			messageToUser.setFill(Color.RED);
+			messageToUser.setText(ipe.getMessage());
+		} catch(Exception exc) {
+			messageToUser.setFill(Color.RED);
+			messageToUser.setText("an error has accured please try again");
 		}
-		else if(female.isSelected()) {
-			gender = Gender.Female;
-		}
-		else {
-			gender = Gender.Unknown;
-		}
-		
-		if(lactose.isSelected())
-			sensitiveLactose = true;
-		if(gluten.isSelected())
-			sensitiveGluten = true;
-		
-		Customer customer = new Customer(firstName.getText(), lastName.getText(), birthDate.getValue(), 
-				gender, NeighberhoodBox.getValue(), sensitiveLactose, sensitiveGluten, userName.getText(), password.getText());	
-		MainController.rest.addCustomer(customer);
-		
-		//new Alert(Alert.AlertType.INFORMATION, "Welcome " + firstName.getText() + ".\n Please log in.").showAndWait();
-		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-		alert.setTitle("Welcome " + firstName.getText());
-		alert.setHeaderText("Welcome " + firstName.getText() + ". \n Please log in to continue.");
-		alert.showAndWait();
-		
-		AnchorPane loginForm = FXMLLoader.load(getClass().getResource(("/View/Login.fxml")));
-        mainScreen.getChildren().setAll(loginForm);
-		/*FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
-		AnchorPane pane = loader.load();
-		mainScreen.getChildren().removeAll(mainScreen.getChildren());
-		mainScreen.getChildren().add(pane);*/
 	}
 	
 	
