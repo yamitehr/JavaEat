931ac050a8dd51971a71b56abcd416ab4c7e0092
yamitehr <yamitehrlich@gmail.com>
2021-08-08 00:31:44 +0300

    Pushed ser file. started to design customer

---
+++ SolEx2/src/Controllers/VideoController.java
@@ -0,0 +1,30 @@
+package Controllers;
+
+import java.io.File;
+
+import javafx.fxml.FXML;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+public class VideoController {
+	private File file;
+	private MediaPlayer mediaPlayer;
+	private Media media;
+	@FXML
+	private MediaView restaurantVideo;
+	
+	@FXML
+	public void initialize() {
+		file = new File("restaurantVideoHD.mp4");
+		media = new Media(file.toURI().toString());
+		mediaPlayer = new MediaPlayer(media);
+		mediaPlayer.play();
+		restaurantVideo.setMediaPlayer(mediaPlayer);
+		mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+	}
+	
+	
+	
+
+}
