d7255912c759e026bc3ea0bfeacef1f6912dec6e
yamitehr <yamitehrlich@gmail.com>
2021-08-19 01:05:35 +0300

    password indicator in tooltip

--- SolEx2/src/Controllers/SignInController.java
+++ SolEx2/src/Controllers/SignInController.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import java.io.File;
 import java.time.LocalDate;
 
 import Exceptions.InvalidInputException;
@@ -8,36 +9,39 @@ import Model.Customer;
 import Model.Restaurant;
 import Utils.Gender;
 import Utils.Neighberhood;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Tooltip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class SignInController extends ControllerWrapper {
 
 	private ObservableList<Neighberhood> neighberhood = FXCollections.observableArrayList(Neighberhood.values());
 	@FXML
 	private ChoiceBox<Neighberhood> NeighberhoodBox;	
-	@FXML
-	private void initialize() {
-		NeighberhoodBox.setItems(neighberhood);
-		returnBtn.setOnAction(e -> {
-			moveToScene("/View/Login.fxml", (Stage)signInBtn.getScene().getWindow(), Consts.defaultWidthLogin, Consts.defaultHeightLogin);
-		});
-	}
+	
 	@FXML
 	private TextField userName;
 	@FXML
@@ -67,6 +71,27 @@ public class SignInController extends ControllerWrapper {
 	@FXML
 	private Button returnBtn;
 	
+	@FXML
+    private Tooltip tooltip;
+    
+    @FXML
+    private ProgressBar passwordMeter;
+   
+    public boolean SHOW_TOOLTIP = false;
+    
+    public static double WEEK = 0.33;
+    public static double MEDIUM = 0.66;
+    public static double STRONG = 1;
+    
+	@FXML
+	private void initialize() {
+		NeighberhoodBox.setItems(neighberhood);
+		returnBtn.setOnAction(e -> {
+			moveToScene("/View/Login.fxml", (Stage)signInBtn.getScene().getWindow(), Consts.defaultWidthLogin, Consts.defaultHeightLogin);
+		});
+		tooltip.setShowDuration(new Duration(100000));
+	}
+
 	
 	public void register(ActionEvent e) throws Exception {
 		try {
@@ -137,6 +162,66 @@ public class SignInController extends ControllerWrapper {
 		}
 	}
 	
+	public static double getStrength(String pw)
+    {
+        if(pw.length() < 3)
+        {
+            return 0;
+        }
+        boolean containsNum = false;
+        boolean containsSpec = false;
+        boolean containsBig = false;
+        for(int i=0;i<pw.length();i++)
+        {
+            if(Character.isDigit(pw.charAt(i)))
+            {
+                containsNum = true;
+            }
+            if(Character.isUpperCase(pw.charAt(i)))
+            {
+                containsBig = true;
+            }
+            if (String.valueOf(pw.charAt(i)).matches("[^a-zA-Z0-9]")) 
+            {
+                containsSpec = true;
+            }
+        }
+        if(containsNum && containsSpec && containsBig)
+        {
+            return STRONG;
+        }
+        else if(containsNum && containsBig || containsNum && containsSpec || containsSpec && containsBig)
+        {
+            return MEDIUM;
+        }
+        
+        return WEEK;
+    }
 	
+	@FXML
+	void setPasswordMeter() {
+	    if(getStrength(password.getText()) == 0 )
+	    {
+	        tooltip.setText("Too short!");
+	    }
+	    else if(getStrength(password.getText()) == WEEK )
+	    {
+	        tooltip.setText("Password is week!");
+	        passwordMeter.setStyle("-fx-accent: red;");
+	    }
+	    else if(getStrength(password.getText()) == MEDIUM )
+	    {
+	        tooltip.setText("Password is medium!");
+	        passwordMeter.setStyle("-fx-accent: orange;");
+	    }
+	    else if(getStrength(password.getText()) == STRONG)
+	    {
+	        tooltip.setText("Password is strong!");
+	        passwordMeter.setStyle("-fx-accent: green;");
+	    }
+	    passwordMeter.setProgress(getStrength(password.getText()));
+	}
+
+	 
 
 }
